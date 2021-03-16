44fe8100dd1fbca0399e8595c8eee095d1e38681
Ryantha Purba <ryantha.purba@gmail.com>
2021-03-16 16:05:43 +0700

    Adding optimize version of trap rainwater

--- array_3_trap_rainwater.js
+++ array_3_trap_rainwater.js
@@ -1,21 +1,48 @@
 function trap_rainwater(arr) {
-  let total = 0,
-    maxR = 0,
-    maxL = 0;
+  // brute force version //
+  // let total = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let l = i - 1; l >= 0; l--) {
-      arr[l] > maxL ? (maxL = arr[l]) : (maxL = maxL);
+  // for (let i = 0; i < arr.length; i++) {
+  //   let maxR = 0,
+  //     maxL = 0,
+  //     l = i - 1,
+  //     r = i + 1;
+
+  //   while (l >= 0) {
+  //     maxL = Math.max(maxL, arr[l]);
+  //     l--;
+  //   }
+
+  //   while (r < arr.length) {
+  //     maxR = Math.max(maxR, arr[r]);
+  //     r++;
+  //   }
+
+  //   console.log({ maxL, maxR });
+  //   const currentWater = Math.min(maxL, maxR) - arr[i];
+  //   currentWater > 0 ? (total += currentWater) : null;
+  //   console.log({ total });
+  // }
+
+  // return total;
+
+  // optimize version //
+  let total = 0,
+    pl = 0,
+    pr = arr.length - 1,
+    maxL = arr[pl],
+    maxR = arr[pr];
+
+  while (pl < pr) {
+    if (arr[pl] <= arr[pr]) {
+      if (maxL > arr[pl]) total += maxL - arr[pl];
+      maxL = Math.max(maxL, arr[pl]);
+      pl++;
+    } else {
+      if (maxR > arr[pr]) total += maxR - arr[pr];
+      maxR = Math.max(maxR, arr[pr]);
+      pr--;
     }
-    for (let r = i + 1; r < arr.length; r++) {
-      arr[r] > maxR ? (maxR = arr[r]) : (maxR = maxR);
-    }
-    console.log({ maxL, maxR });
-    const currentWater = Math.min(maxL, maxR) - arr[i];
-    currentWater > 0 ? (total += currentWater) : null;
-    maxR = 0;
-    maxL = 0;
-    console.log({ total });
   }
 
   return total;
