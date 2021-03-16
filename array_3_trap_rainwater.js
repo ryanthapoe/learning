e973f080bc69ab0ac8572f6434fb259dd83f8508
Ryantha Purba <ryantha.purba@gmail.com>
2021-03-16 14:03:43 +0700

    Fixing brute force trap rainwater

--- array_3_trap_rainwater.js
+++ array_3_trap_rainwater.js
@@ -1,26 +1,33 @@
 function trap_rainwater(arr) {
   let max_rainwater = 0,
-    temp_rainwater = 0,
+    temp_block = [],
     currentBlock = 0;
-
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > currentBlock || i === arr.length - 1) {
+      const blok_terpendek = Math.min(currentBlock, arr[i]);
+      let temp_rainwater;
+      temp_block.length > 0
+        ? (temp_rainwater = temp_block.reduce(
+            (a, b) => a + (blok_terpendek - b)
+          ))
+        : (temp_rainwater = 0);
       max_rainwater = Math.max(max_rainwater, temp_rainwater);
-      temp_rainwater = 0;
+      temp_block = [];
       currentBlock = arr[i];
     } else {
-      temp_rainwater += currentBlock - arr[i];
+      temp_block.push(arr[i]);
     }
   }
-
   return max_rainwater;
 }
 
 const testOne = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
 const testTwo = [0];
 const testThree = [];
-const TestFour = [4, 8, 4];
+const testFour = [4, 8, 4];
+const testFive = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
 console.log(trap_rainwater(testOne));
 console.log(trap_rainwater(testTwo));
 console.log(trap_rainwater(testThree));
-console.log(trap_rainwater(TestFour));
+console.log(trap_rainwater(testFour));
+console.log(trap_rainwater(testFive));
