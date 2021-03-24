3a8a99b2662c72dfbb94453ff3f4e09acda94f0e
Ryantha Purba <ryantha.purba@gmail.com>
2021-03-24 12:18:46 +0700

    Completing day1 hash table

---
+++ Data_Structures/Hash_Table_Training/Day1.js
@@ -0,0 +1,43 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  get(key) {
+    const hashAddr = this._hash(key);
+    const bucket = this.data[hashAddr];
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) return bucket[i][1];
+    }
+    return undefined;
+  }
+
+  set(key, value) {
+    const hashAddr = this._hash(key);
+    if (!this.data[hashAddr]) this.data[hashAddr] = [];
+    this.data[hashAddr].push([key, value]);
+    return hashAddr;
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % key.length;
+    }
+    return hash;
+  }
+}
+
+const testing = new HashTable(10);
+console.log(testing.set("grapes", 1000));
+console.log(testing.set("apples", 1));
+console.log(testing.set("orange", 1));
+console.log(testing.set("watermelon", 31));
+console.log(testing.set("mango", 0));
+console.log(testing.set("pineApple", 32));
+console.log(testing.set("Nanas", 33));
+console.log(testing);
+console.log(testing.get("grapes"));
+console.log(testing.get("watermelon"));
+console.log(testing.get("pineApple"));
+console.log(testing.get("Nanas"));
