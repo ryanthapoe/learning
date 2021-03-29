02a531b9fe74eaa36a5a744db59abf6914798ed4
Ryantha Purba <ryantha.purba@gmail.com>
2021-03-29 15:43:49 +0700

    Adding Linked List Data Structures

--- Data_Structures/LinkedList.js
+++ Data_Structures/LinkedList.js
@@ -1,38 +1,36 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
-    this.head = {
-      value,
-      next: null,
-    };
+    this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
   }
 
   append(value) {
-    const newValue = {
-      value,
-      next: null,
-    };
-    this.tail.next = newValue;
+    const newNode = new Node(value);
+    this.tail.next = newNode;
     this.tail = this.tail.next;
     this.length++;
     return this.length;
   }
 
   prepend(value) {
-    const newValue = {
-      value,
-      next: this.head,
-    };
-    this.head = newValue;
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
     this.length++;
     return this.length;
   }
 
   lookup(value) {
-    let found = false;
     let current = this.head;
-    while (!found) {
+    while (true) {
       if (current.value === value) {
         return current;
       }
@@ -43,15 +41,13 @@ class LinkedList {
   insert(index, value) {
     if (index === 0) return this.prepend(value);
     if (index >= this.length) return "Index undefined";
+    const newNode = new Node(value);
     let current = this.head;
     for (let i = 1; i < index; i++) {
       current = current.next;
     }
-    const newValue = {
-      value,
-      next: current.next,
-    };
-    current.next = newValue;
+    newNode.next = current.next;
+    current.next = newNode;
     this.length++;
     return this.length;
   }
@@ -71,18 +67,28 @@ class LinkedList {
     this.length--;
     return current.value;
   }
+
+  printList() {
+    const data = [];
+    let current = this.head;
+    while (current !== null) {
+      data.push(current.value);
+      current = current.next;
+    }
+    return data.toString();
+  }
 }
 
 const tester = new LinkedList(10);
-console.log(tester);
+console.log(tester.printList());
 console.log(tester.append(5));
-console.log(tester);
+console.log(tester.printList());
 console.log(tester.append(16));
-console.log(tester);
+console.log(tester.printList());
 console.log(tester.prepend(12));
-console.log(tester);
+console.log(tester.printList());
 console.log(tester.lookup(10));
 console.log(tester.insert(2, 8));
-console.log(tester.head);
+console.log(tester.printList());
 console.log(tester.delete(1));
-console.log(tester.head);
+console.log(tester.printList());
