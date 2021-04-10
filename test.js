bee8f8255a2a6e589b41fd69f17d4759dad78303
Ryantha Purba <ryantha.purba@gmail.com>
2021-04-10 14:54:52 +0700

    Completing Array Training Day14

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-let restaurant = {
-  "apa bisa": "bisa",
-  test: "tester",
-};
-
-console.log(restaurant["test"]);
+async function getTotalGoals(team, year) {
+  const url1 = `http://jsonmock.hackerrank.com/api/football_matches?year=${year}&team1=${team}&page=1`;
+  const url2 = `http://jsonmock.hackerrank.com/api/football_matches?year=${year}&team2=${team}&page=1`;
+  const rawMatches = await fetch(url1);
+  const matches = await rawMatches.json();
+  console.log(matches);
+}
