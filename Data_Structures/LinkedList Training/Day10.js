22ec871905ea321180999c63c58ae1ecd7a53f5e
Ryantha Purba <ryantha.purba@gmail.com>
2021-04-13 12:06:16 +0700

    Completing Day10 LinkedList

---
+++ Data_Structures/LinkedList Training/Day10.js
@@ -0,0 +1,85 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this.print();
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this.print();
+  }
+
+  print() {
+    const data = [];
+    let current = this.head;
+    while (current != null) {
+      data.push(current.value);
+      current = current.next;
+    }
+    return data;
+  }
+
+  lookup(index) {
+    const data = this.traverseIn(index);
+    return data.value;
+  }
+
+  traverseIn(index) {
+    let current = this.head;
+    for (let i = 1; i <= index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  insert(index, value) {
+    if (index === 0) return this.prepend(value);
+    if (index >= this.length) return this.append(value);
+    const newNode = new Node(value);
+    const leader = this.traverseIn(index - 1);
+    const next = leader.next;
+    leader.next = newNode;
+    newNode.next = next;
+    this.length++;
+    return this.print();
+  }
+
+  remove(index) {
+    const leader = this.traverseIn(index - 1);
+    const removed = leader.next;
+    leader.next = removed.next;
+    this.length--;
+    return this.print();
+  }
+}
+
+const tester = new LinkedList(10);
+console.log(tester.append(5));
+console.log(tester.append(16));
+console.log(tester.prepend(12));
+console.log(tester.lookup(2));
+console.log(tester.insert(0, 8));
+console.log(tester.insert(888, 555));
+console.log(tester.insert(2, "deleted"));
+console.log(tester.remove(2));
+console.log(tester.remove(tester.length - 1));
+console.log(tester.length);
