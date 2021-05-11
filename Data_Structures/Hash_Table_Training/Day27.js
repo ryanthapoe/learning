462c73d2f1c3ac17606aa24f3814314f7c0964d2
Ryantha Purba <ryantha.purba@gmail.com>
2021-05-11 09:27:13 +0700

    Completing Day27 HashTable

---
+++ Data_Structures/Hash_Table_Training/Day27.js
@@ -0,0 +1,66 @@
+class HashTable {
+  constructor(size = 50) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i)) % key.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    const address = this._hash(key);
+    if (!this.data[address]) this.data[address] = [];
+    this.data[address].push([key, value]);
+    return this.print();
+  }
+
+  get(key) {
+    const address = this._hash(key);
+    const bucket = this.data[address];
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) return bucket[i][1];
+      }
+    }
+    return undefined;
+  }
+
+  print() {
+    return this.getValues(1);
+  }
+
+  keys() {
+    return this.getValues(0);
+  }
+
+  getValues(index) {
+    const data = [];
+    for (let i = 0; i < this.data.length; i++) {
+      const bucket = this.data[i];
+      if (bucket) {
+        for (let j = 0; j < bucket.length; j++) {
+          if (bucket[j][index] !== undefined) data.push(bucket[j][index]);
+        }
+      }
+    }
+    return data;
+  }
+}
+
+const tester = new HashTable();
+console.log(tester.set("grapes", 1000));
+console.log(tester.set("apples", 1));
+console.log(tester.set("orange", 2));
+console.log(tester.set("watermelon", 31));
+console.log(tester.set("mango", 0));
+console.log(tester.set("pineApple", 32));
+console.log(tester.set("Nanas", 33));
+console.log(tester.get("grapes"));
+console.log(tester.get("watermelon"));
+console.log(tester.get("pineApple"));
+console.log(tester.get("Nanas"));
+console.log(tester.keys());
